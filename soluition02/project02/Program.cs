17ca13e39029add04f0f2635b485477be744105b
Malakaka <malakalsenine@gmail.com>
2026-04-15 14:33:57 +0400

    Day 2 (Update Task1 - Day Name Printer)

--- soluition02/project02/Program.cs
+++ soluition02/project02/Program.cs
@@ -127,4 +127,16 @@ int day = Convert.ToInt32(Console.ReadLine());
 switch (day)
 {
 
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+
 }
