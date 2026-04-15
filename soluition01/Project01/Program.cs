40d8a56537bdb2e96590b6749202ff57ca00b883
Malakaka <malakalsenine@gmail.com>
2026-04-15 11:34:32 +0400

    Day 1 Task

---
+++ soluition01/Project01/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter First Number !");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Second Number !");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // (+)
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+
+            // (*)
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+
+            // (/)
+            Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+
+            // (-)
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+        }
+    }
+}
