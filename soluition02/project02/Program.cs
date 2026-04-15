b5e053527abeebb7ebb9ad30ddf1a95c49b15714
Malakaka <malakalsenine@gmail.com>
2026-04-15 12:26:15 +0400

    Day 2 Task 1

--- soluition02/project02/Program.cs
+++ soluition02/project02/Program.cs
@@ -5,6 +5,26 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //Task 1- Positive, Negative, or Zero 
+
+            Console.WriteLine("Please Enter Any Number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            if (Number > 0)
+            {
+                Console.WriteLine("Positive");
+            }
+
+            else if (Number < 0)
+            {
+                Console.WriteLine("Negative");
+            }
+
+            else
+            {
+                Console.WriteLine("Zero");
+            }
         }
     }
 }
