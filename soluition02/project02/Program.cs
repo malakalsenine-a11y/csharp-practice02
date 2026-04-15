52bd3c51f9b0078d26908d072dfd8b89e7505bac
Malakaka <malakalsenine@gmail.com>
2026-04-15 12:29:57 +0400

    Day 2 Task 3

--- soluition02/project02/Program.cs
+++ soluition02/project02/Program.cs
@@ -40,7 +40,34 @@
                 Console.WriteLine("Odd Number");
             }
 
+            //Task3 - Student Grade System:
+            Console.WriteLine("Please Enter Student Score From 0 to 100");
+            int Score = Convert.ToInt32(Console.ReadLine());
 
+            if (Score >= 90 && Score <= 100)
+            {
+                Console.WriteLine("Excellent");
+            }
+
+            else if (Score >= 75 && Score <= 89)
+            {
+                Console.WriteLine("Very Good");
+            }
+
+            else if (Score >= 60 && Score <= 74)
+            {
+                Console.WriteLine("Good");
+            }
+
+            else if (Score >= 50 && Score <= 59)
+            {
+                Console.WriteLine("Pass");
+            }
+
+            else
+            {
+                Console.WriteLine("Fail");
+            }
         }
     }
 }
