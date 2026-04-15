303392b0b788e34b66a991fa3807b19f63080f27
Malakaka <malakalsenine@gmail.com>
2026-04-15 12:02:05 +0400

    *

--- soluition01/Project01/Program.cs
+++ soluition01/Project01/Program.cs
@@ -12,6 +12,9 @@
 
             // (+)
             Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+
+            // (*)
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
         }
     }
 }
