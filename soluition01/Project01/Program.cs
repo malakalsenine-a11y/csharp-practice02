99a34bbffa41d15127248c2388f65a09896a8786
Malakaka <malakalsenine@gmail.com>
2026-04-15 11:38:46 +0400

    FirST

--- soluition01/Project01/Program.cs
+++ soluition01/Project01/Program.cs
@@ -7,20 +7,7 @@
             Console.WriteLine("Please Enter First Number !");
             int num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please Enter Second Number !");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-
-            // (+)
-            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
-
-            // (*)
-            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
-
-            // (/)
-            Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
-
-            // (-)
-            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+         
         }
     }
 }
