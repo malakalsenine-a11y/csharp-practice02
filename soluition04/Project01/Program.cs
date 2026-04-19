a0d18fa789b45dfc958407132a14307ff1264629
Malakaka <malakalsenine@gmail.com>
2026-04-19 09:17:25 +0400

    Soluition04 for day4

---
+++ soluition04/Project01/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+
+        }
+    }
+}
