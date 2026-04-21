820b8b6c9c42cfc2478f89c5324eade1cac00209
Malakaka <malakalsenine@gmail.com>
2026-04-21 12:37:17 +0400

    Soluition Part 2 – Student Grades (Multi-Dimensional Array)

--- soluition06/project01/Program.cs
+++ soluition06/project01/Program.cs
@@ -6,54 +6,95 @@
         {
             #region Number of array:
 
-            Console.WriteLine("Enter Number of Rows: ");
-            int rows = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Number of Rows: ");
+            //int rows = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter Number of Columns: ");
-            int columns = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Number of Columns: ");
+            //int columns = Convert.ToInt32(Console.ReadLine());
 
-            int[,] numbers = new int[rows, columns];
+            //int[,] numbers = new int[rows, columns];
 
-            for (int x = 0; x < rows; x++)
+            //for (int x = 0; x < rows; x++)
+            //{
+            //    for (int i = 0; i < columns; i++)
+            //    {
+            //        Console.WriteLine($"Enter Value For ({x},{i}) : ");
+            //        numbers[x, i] = Convert.ToInt32(Console.ReadLine());
+
+            //    }
+
+            //}
+
+            //Console.WriteLine("The Number of Array is:");
+            //for (int x = 0; x < rows; x++)
+            //{
+            //    for (int i = 0; i < columns; i++)
+            //    {
+            //        Console.Write(numbers[x, i] + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region Part01 - Student Names(One Dimensional Array)
+
+            string[] names = new string[5];
+
+
+            for (int i = 0; i < 5; i++)
             {
-                for (int i = 0; i < columns; i++)
-                {
-                    Console.WriteLine($"Enter Value For ({x},{i}) : ");
-                    numbers[x, i] = Convert.ToInt32(Console.ReadLine());
-
-                }
-
+                Console.Write($"Enter name of student {i + 1}: ");
+                names[i] = Console.ReadLine();
             }
 
-            Console.WriteLine("The Number of Array is:");
-            for (int x = 0; x < rows; x++)
+            Console.WriteLine("\nStudent Names:");
+
+            for (int i = 0; i < 5; i++)
             {
-                for (int i = 0; i < columns; i++)
+                Console.WriteLine(names[i]);
+            }
+
+            #endregion
+
+            #region Part 2 – Student Grades (Multi-Dimensional Array)
+
+            //  الدرجات
+            int[,] grades = new int[5, 3];
+
+            string[] subjects = { "Math", "Science", "English" };
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Grades for {names[i]}:");
+
+                for (int j = 0; j < 3; j++)
                 {
-                    Console.Write(numbers[x, i] + " ");
+                    Console.Write($"Enter {subjects[j]} grade: ");
+                    grades[i, j] = Convert.ToInt32(Console.ReadLine());
                 }
+            }
+
+            Console.WriteLine("Final Result:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(names[i] + ": ");
+
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(grades[i, j] + " ");
+                }
+
                 Console.WriteLine();
             }
-            #endregion
-
-            #region Part01 - Student Names:
-
-            string[] student = new string[5];
-
-            for (int x = 0; x < student.Length; x++ )
-            {
-                Console.WriteLine("Enter Name for the students : ");
-                student[x] = Console.ReadLine();
-            }
-
-            Console.WriteLine("Students Name ;");
-            for (int x = 0; x < student.Length; x++ )
-            {
-                Console.WriteLine(student[x]);
-            }
+        
 
             #endregion
 
+
+
+
+}
         }
     }
-}
+
