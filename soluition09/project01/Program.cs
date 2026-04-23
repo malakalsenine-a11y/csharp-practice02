168e044f2f0b8b1f27ddc1b53ce9f4d0722efdc7
Malakaka <malakalsenine@gmail.com>
2026-04-23 14:21:04 +0400

    Create Calculator

---
+++ soluition09/project01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator();
+        }
+
+        static void Calculator()
+        {
+
+        }
+    }
+}
