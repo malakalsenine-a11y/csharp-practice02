e2f71584e149373fa32be3cb95fb48c48f85e830
Malakaka <malakalsenine@gmail.com>
2026-04-23 14:23:22 +0400

    Create Calculator

--- soluition09/project01/Program.cs
+++ soluition09/project01/Program.cs
@@ -26,8 +26,38 @@ namespace project01
 
                 double result = 0;
 
-               
-        
-    }
+                switch (op)
+                {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+
+                    case '-':
+                        result = num1 - num2;
+                        break;
+
+                    case '*':
+                        result = num1 * num2;
+                        break;
+
+                    case '/':
+                        if (num2 != 0)
+                            result = num1 / num2;
+                        else
+                        {
+                            Console.WriteLine("Cannot divide by zero!");
+                            result = 0;
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid operation!");
+                        result = 0;
+                        break;
+                }
+
+                
+
+        }
     }
 }
