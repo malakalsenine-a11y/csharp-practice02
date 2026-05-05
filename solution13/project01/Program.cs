d27f6ce8cc089438a3dc20f039fe8b2d4af21cd7
Malakaka <malakalsenine@gmail.com>
2026-05-05 09:40:02 +0400

    Class and Object

--- solution13/project01/Program.cs
+++ solution13/project01/Program.cs
@@ -2,9 +2,29 @@
 {
     internal class Program
     {
+        class Car
+        {
+            public string color;
+            public int model_year {  get; }
+            private int temp = 10;
+            
+            public int Speed
+            {
+                get { return temp; }
+                set
+                {
+                    if (value > 0)
+                    {
+                        temp = value;
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car my_car = new Car();
+            Console.WriteLine(my_car.Speed);
         }
     }
 }
--- solution13/project01/obj/Debug/net8.0/project01.AssemblyInfo.cs
+++ solution13/project01/obj/Debug/net8.0/project01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6830f9724aa6abb924b7a0fcdcd67064a9acebaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c9727b46dc4b7e18f568558117824f602bd8027")]
 [assembly: System.Reflection.AssemblyProductAttribute("project01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
