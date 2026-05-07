7454f03692280542c1f19749e8e7cd1858566193
Malakaka <malakalsenine@gmail.com>
2026-05-07 10:31:12 +0400

    Add inheritance and method overriding example in C#

--- soluition14/project01/Program.cs
+++ soluition14/project01/Program.cs
@@ -2,9 +2,55 @@
 {
     internal class Program
     {
+
+        class Parent
+        {
+            public void Message()
+            {
+                Console.WriteLine("This is normal function from parent");
+            }
+
+            // Virtual Function 1:
+            public virtual void Display()
+            {
+                Console.WriteLine("Display function from Parent");
+            }
+
+            // Virtual Function 2:
+            public virtual void Show()
+            {
+                Console.WriteLine("Show function from Parent");
+            }
+
+        }
+
+        class Child : Parent
+        {
+            // override  Function 1:
+            public override void Display()
+            {
+                Console.WriteLine("Display function overridden in Child");
+            }
+
+            // override  Function 2:
+            public override void Show()
+            {
+                Console.WriteLine("Show function overridden in Child");
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create object from Child
+            Child obj = new Child();
+
+            // Call inherited normal function
+            obj.Message();
+
+            // Call overridden functions
+            obj.Display();
+            obj.Show();
         }
     }
 }
--- soluition14/project01/obj/Debug/net8.0/project01.AssemblyInfo.cs
+++ soluition14/project01/obj/Debug/net8.0/project01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d27f6ce8cc089438a3dc20f039fe8b2d4af21cd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e934acb7cf98e109b467d82a0c4aa43f0b02a26")]
 [assembly: System.Reflection.AssemblyProductAttribute("project01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
