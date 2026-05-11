507bbb8181057f022eb974d0d4cfaa661f9649c8
Malakaka <malakalsenine@gmail.com>
2026-05-11 08:48:38 +0400

    Create new soluition for a new day16

---
+++ soluition15/project01/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace OOPArrays
+{
+    class Student
+    {
+        public string Name;
+        public int Grade;
+
+        public void Display()     // دالة العرض
+        {
+            Console.WriteLine($"Name: {Name}, Grade: {Grade}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // إنشاء Array من نوع Student
+            Student[] students = new Student[3];
+
+            // إضافة Objects داخل الـ Array
+            students[0] = new Student();
+            students[0].Name = "Ali";
+            students[0].Grade = 90;
+
+            students[1] = new Student();
+            students[1].Name = "Sara";
+            students[1].Grade = 85;
+
+            students[2] = new Student();
+            students[2].Name = "Omar";
+            students[2].Grade = 95;
+
+            // طباعة البيانات
+            foreach (Student s in students)
+            {
+                s.Display();
+            }
+        }
+    }
+}
+
+
+//==========================================================================================================================
+
+using System;
+using System.Collections.Generic;
+
+namespace OOPList
+{
+    class Student
+    {
+        public string Name;
+        public int Grade;
+
+        public void Display()
+        {
+            Console.WriteLine($"Name: {Name}, Grade: {Grade}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<Student> students = new List<Student>();
+
+            
+            students.Add(new Student { Name = "Ali", Grade = 90 });
+            students.Add(new Student { Name = "Sara", Grade = 85 });
+            students.Add(new Student { Name = "Omar", Grade = 95 });
+
+            
+            foreach (Student s in students)
+            {
+                s.Display();
+            }
+        }
+    }
+}
---
+++ soluition16/project01/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+
+        class Student
+        {
+            public string Name;
+        }
+
+        class Teacher
+        {
+            public void Teach(Student s)
+            {
+                Console.WriteLine("Teacher teaches " + s.Name);
+            }
+        }
+
+
+       
+        static void Main(string[] args)
+        {
+            Student st = new Student();
+            st.Name = "Malak";
+
+            Teacher t = new Teacher();
+
+            // Association
+            t.Teach(st);
+        }
+    }
+}
